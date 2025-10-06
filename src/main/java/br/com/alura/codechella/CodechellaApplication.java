4c5badc9198c01215f85b9799159990eae3a728a
RenanCruz7 <renancruz199@gmail.com>
2025-10-06 19:23:58 -0300

    Initial Commit

--- src/main/java/br/com/alura/codechella/CodechellaApplication.java
+++ src/main/java/br/com/alura/codechella/CodechellaApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.codechella;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodechellaApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("src")
+				.filename(".env")
+				.load();
+
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(CodechellaApplication.class, args);
 	}
 
