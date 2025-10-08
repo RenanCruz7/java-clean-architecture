847f9ee5d311693902e462dfc99df80d69be4a79
RenanCruz7 <renancruz199@gmail.com>
2025-10-08 20:30:24 -0300

    Feat: Adicionando mapeamento entre Usuario e UsuarioEntity no RepositorioDeUsuarioJpa

--- src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioJpa.java
+++ src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioJpa.java
@@ -2,18 +2,23 @@ package br.com.alura.codechella.infra.gateways;
 
 import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
 import br.com.alura.codechella.domain.Usuario;
+import br.com.alura.codechella.infra.persistence.UsuarioEntity;
 import br.com.alura.codechella.infra.persistence.UsuarioRepository;
 
 public class RepositorioDeUsuarioJpa implements RepositorioDeUsuario {
 
     private final UsuarioRepository repositorio;
+    private final UsuarioEntityMapper usuarioEntityMapper;
 
-    public RepositorioDeUsuarioJpa(UsuarioRepository repositorio) {
+    public RepositorioDeUsuarioJpa(UsuarioRepository repositorio, UsuarioEntityMapper usuarioEntityMapper) {
+        this.usuarioEntityMapper = usuarioEntityMapper;
         this.repositorio = repositorio;
     }
 
     @Override
     public Usuario cadastrarUsuario(Usuario usuario) {
-        return repositorio.save((usuario));
+        UsuarioEntity usuarioEntity = usuarioEntityMapper.toEntity(usuario);
+        repositorio.save((usuarioEntity));
+        return usuarioEntityMapper.toDomain(usuarioEntity);
     }
 }
